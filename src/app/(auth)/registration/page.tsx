2eacb3b60db87913fa22dd4451073ecc34bc47fb
Desu Chan <orenodesukouichi@gmail.com>
2024-05-11 14:23:51 +0800

    registration page wip

---
+++ src/app/(auth)/registration/page.tsx
@@ -0,0 +1,18 @@
+import Container from '@/components/layouts/web/Container'
+import { FC } from 'react'
+
+interface pageProps {
+
+}
+
+const page: FC<pageProps> = ({ }) => {
+    return (
+        <Container>
+            <div>
+                page
+            </div>
+        </Container>
+    )
+}
+
+export default page
