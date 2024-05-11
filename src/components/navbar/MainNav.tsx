1f5e9046e606d53629b92724a1d06ce0f43dd56e
Desu Chan <orenodesukouichi@gmail.com>
2024-05-11 14:24:15 +0800

    main nav wip

---
+++ src/components/navbar/MainNav.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { FC } from 'react'
+
+interface MainNavProps {
+
+}
+
+const MainNav: FC<MainNavProps> = ({ }) => {
+    return <div>MainNav</div>
+}
+
+export default MainNav
