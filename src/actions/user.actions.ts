753d8db66613c313565f57ec59887120df154e78
Desu Chan <orenodesukouichi@gmail.com>
2024-05-17 01:49:08 +0800

    moved editprofile

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -5,40 +5,6 @@ import { avatarFormSchema, bioFormSchema, userSettingsSchema } from "@/schema";
 import { db } from '@/lib/db';
 import { validateRequest } from '@/auth';
 
-export const editProfile = async (values: z.infer<typeof userSettingsSchema>) => {
-
-    const { name, username, bio, password, newPassword } = values
-
-    const session = await validateRequest()
-    const sessionId = session.user?.id
-
-    const existingUser = await db.user.findFirst({
-        where: {
-            id: sessionId
-        }
-    })
-
-    if (!existingUser) {
-        return {
-            error: 'Unauthorized'
-        }
-    } else {
-        await db.user.update({
-            where: {
-                id: sessionId
-            },
-            data: {
-                name,
-                username,
-                bio
-            }
-        })
-        return {
-            success: 'Profile updated successfully'
-        }
-    }
-}
-
 export const uploadAvatar = async (values: z.infer<typeof avatarFormSchema>) => {
 
     const { imageUrl } = values
@@ -100,6 +66,43 @@ export const addBio = async (values: z.infer<typeof bioFormSchema>) => {
         })
         return {
             success: 'Bio added successfully'
+        }
+    }
+}
+
+export const addService = async (values: z.infer<typeof bioFormSchema>) => {
+}
+
+export const editProfile = async (values: z.infer<typeof userSettingsSchema>) => {
+
+    const { name, username, bio, password, newPassword } = values
+
+    const session = await validateRequest()
+    const sessionId = session.user?.id
+
+    const existingUser = await db.user.findFirst({
+        where: {
+            id: sessionId
+        }
+    })
+
+    if (!existingUser) {
+        return {
+            error: 'Unauthorized'
+        }
+    } else {
+        await db.user.update({
+            where: {
+                id: sessionId
+            },
+            data: {
+                name,
+                username,
+                bio
+            }
+        })
+        return {
+            success: 'Profile updated successfully'
         }
     }
 }
