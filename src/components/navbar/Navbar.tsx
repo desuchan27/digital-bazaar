e11e10a11cbadbb311a5fcf6b76f4f361a84afa4
Desu Chan <orenodesukouichi@gmail.com>
2024-05-18 02:58:36 +0800

    fixing navbar padding

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar: FC<NavbarProps> = ({ }) => {
     return (
         <div className='w-full fixed top-0 left-0 z-50 backdrop-blur border-b bg-slate-500 bg-clip-padding backdrop-filter bg-opacity-50 backdrop-saturate-100 backdrop-contrast-100' >
             <Container>
-                <div className='text-primary-foreground flex flex-row w-full items-center justify-between h-[64px] '>
+                <div className='text-primary-foreground flex flex-row w-full items-center justify-between h-[64px] px-4 xl:px-0'>
                     <Link href='/'>
                         <h2 className='font-semibold'>Digital Bazaaar</h2>
                     </Link>
